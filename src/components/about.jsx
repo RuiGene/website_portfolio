12ed9fff11d8f3cafcab73249d16b2260791ebdd
RuiGene <danielyangnz@hotmail.com>
2023-07-29 23:46:49 +1200

    added about section

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -4,12 +4,23 @@ const About = () => {
   return (
     <div name='about' className='w-full h-screen bg-[#0a192f] text-gray-300'>
         <div className='flex flex-col justify-center items-center w-full h-full'>
-            <div>
-                <div>
-                    <p>About</p>
+            <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+                <div className='sm:text-right pb-8 pl-4'>
+                    <p className='text-4xl font-bold inline border-b-4 border-pink-600'>
+                        About
+                    </p>
                 </div>
                 <div>
-                    
+                </div>
+                <div className='max-w-[1000px] w-full gird sm:grid-cols-2 gap-8 px-4 '>
+                    <div className='sm:text-right text-4xl font-bold'>
+                        <p>Hi. I'm Daniel, nice to meet you</p>
+                    </div>
+                    <div>
+                        <p>
+                            sdjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjl
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
