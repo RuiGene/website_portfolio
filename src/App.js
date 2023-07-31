316045702e213d47e3fbb1eccb131b53f7b3012f
RuiGene <danielyangnz@hotmail.com>
2023-07-31 22:52:39 +1200

    added work section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import About from "./components/about";
 import Home from "./components/home";
 import Skills from "./components/Skills";
+import Work from "./components/work";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Home />
       <About />
       <Skills />
+      <Work />
     </div>
   );
 }
